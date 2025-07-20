1b2beef6dd6321cf7aaa534fcb94875659989808
Abdul Kaba <kababdoul@hotmail.com>
2025-07-20 16:59:31 +0000

    deleted a sitting duck

--- tests/FluentAnnotationsValidator.AspNetCore.Tests/Validators/LoginDtoValidator.cs
+++
@@ -1,8 +0,0 @@
-using FluentAnnotationsValidator.AspNetCore.Tests.Models;
-
-namespace FluentAnnotationsValidator.AspNetCore.Tests.Validators;
-
-public class LoginDtoValidator(IValidationMessageResolver resolver) 
-: DataAnnotationsValidator<LoginDto>(resolver)
-{
-}
