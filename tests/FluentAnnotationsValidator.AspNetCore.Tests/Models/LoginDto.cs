c1c2ddd4e472236c9fcb16115922fdfeb1b4ea7b
Abdul Kaba <kababdoul@hotmail.com>
2025-07-20 16:08:01 +0000

    convert class to record

--- tests/FluentAnnotationsValidator.AspNetCore.Tests/Models/LoginDto.cs
+++ tests/FluentAnnotationsValidator.AspNetCore.Tests/Models/LoginDto.cs
@@ -2,8 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FluentAnnotationsValidator.AspNetCore.Tests.Models;
 
-public class LoginDto
-{
-    [Required, EmailAddress] public string Email { get; set; } = default!;
-    [Required] public string Password {  get; set; } = default!;
-}
+public record LoginDto
+(
+    [Required, EmailAddress] string Email,
+    [Required] string Password,
+    string? Role = null
+);
