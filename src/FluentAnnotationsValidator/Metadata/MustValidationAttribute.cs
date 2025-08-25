499c0f35bbd2838a33bdfebfae502eee1c364f5f
Abdul Kaba <kababdoul@hotmail.com>
2025-08-25 18:37:17 +0000

    add XML documentation

--- src/FluentAnnotationsValidator/Metadata/MustValidationAttribute.cs
+++ src/FluentAnnotationsValidator/Metadata/MustValidationAttribute.cs
@@ -2,6 +2,14 @@
 
 namespace FluentAnnotationsValidator.Metadata;
 
+/// <summary>
+/// Initializes a new instance of the <see cref="MustValidationAttribute{TProperty}"/> class.
+/// </summary>
+/// <typeparam name="TProperty">The type of the property the predicate operates on.</typeparam>
+/// <param name="predicate">A function that performs the validation.</param>
+/// <remarks>
+/// This class represents an object that performs validation using the predicate specified in the constructor.
+/// </remarks>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
 internal class MustValidationAttribute<TProperty>(Func<TProperty, bool> predicate) : ValidationAttribute
 {
