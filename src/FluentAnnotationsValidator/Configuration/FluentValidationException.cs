1381cfd063a0fcdfb42631354794cc13a939ade4
Abdul Kaba <kababdoul@hotmail.com>
2025-09-23 23:33:53 +0000

    add FluentValidationException class

---
+++ src/FluentAnnotationsValidator/Configuration/FluentValidationException.cs
@@ -0,0 +1,32 @@
+﻿namespace FluentAnnotationsValidator.Configuration;
+
+/// <summary>
+/// Represents errors that occur during fluent validation configuration or execution.
+/// </summary>
+public class FluentValidationException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FluentValidationException"/> class.
+    /// </summary>
+    public FluentValidationException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FluentValidationException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    public FluentValidationException(string? message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FluentValidationException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
+    public FluentValidationException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+}
